ecedcd3b8ff7347e5192e1e32ac5f4ed0e89502b
jonnyboi <jonathan.wright@networkrail.co.uk>
2022-10-16 20:22:51 +0100

    client

--- src/utils/explorer.ts
+++ src/utils/explorer.ts
@@ -12,12 +12,12 @@ export function getExplorerUrl(
         cluster = `custom&customUrl=${encodeURIComponent(
           'http://127.0.0.1:8899'
         )}`
-      } else if (endpoint === 'https://api.devnet.solana.com') {
+      } else if (endpoint === 'https://api.devnet.safecoin.org') {
         cluster = 'devnet'
       }
   
       return cluster ? `?cluster=${cluster}` : ''
     }
   
-    return `https://explorer.solana.com/${itemType}/${viewTypeOrItemAddress}${getClusterUrlParam()}`
+    return `https://explorer.safecoin.org/${itemType}/${viewTypeOrItemAddress}${getClusterUrlParam()}`
   }
